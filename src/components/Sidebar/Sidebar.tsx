50e5de62084e74407d391453d92912b5ff6bd3a1
antoniocaldas <j.caldas@rpalatam.com.pe>
2024-03-07 17:37:23 -0500

    change(Sidebar): the state is managed by reference and no longer by the useEffect syles(sidebar): color and styles in sidebar__list

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,50 +1,39 @@
 import { useEffect, useState } from "react";
 import { ApiData, RecordNote } from "../../types/sidebar.types";
 import "./sidebar.scss";
+import { JSONAPI, StaticURL } from "../../types/url.types";
 
 export const Sidebar = () => {
-  const [data, setData] = useState<RecordNote[]>([]);
-  const [visibleData, setVisibleData] = useState<RecordNote[]>([]);
+  const [dataNote, setDataNote] = useState<RecordNote[]>([]);
   const [showAll, setShowAll] = useState(false);
 
-  const updateVisibleData = (showAll: boolean, fullData: RecordNote[]) => {
-    // Me permite calcular la mitad de la data
-    const halfIndex = Math.ceil(fullData.length / 2);
-
-    // Actualiza los datos visibles según el estado showAll y la posición de la mitad
-    setVisibleData(showAll ? fullData : fullData.slice(0, halfIndex));
-  };
-
-  const handleToggleNews = () => {
-    setShowAll((prevShowAll) => !prevShowAll);
-  };
+  //handle para cambiar el estado
+  const handleToggleNews = () => setShowAll((prevShowAll) => !prevShowAll);
+  //con ello puedo cambiar los datos que seran visibles colocando los datos por referencia ya no por useEffect
+  const visibleData = showAll ? dataNote : dataNote?.slice(0, 5);
   const fetchData = async () => {
-    const response = await fetch(
-      "https://api.jsonbin.io/v3/b/63654b012b3499323bf58225"
-    );
+    const url: StaticURL = JSONAPI;
+
+    const response = await fetch(url);
     if (!response.ok) {
       throw new Error("Error fetching data");
     }
     const result: ApiData = await response.json();
-    setData(result.record.notes);
+    setDataNote(result?.record?.notes);
   };
-  //Aqui separo en dos useEffect
-  // La primera para poder realizar el fetching de datos ni bien se monte el componente
+
+  // Hook para el fetching de datos ni bien se monte el componente
   useEffect(() => {
     fetchData();
   }, []);
-  //El segundo es para que si showAll cambia entonces poder ver los elementos restantes
-  useEffect(() => {
-    updateVisibleData(showAll, data);
-  }, [showAll, data]);
 
   return (
     <div className="sidebar">
       <h2 className="sidebar__title">ÚLTIMAS NOTICIAS</h2>
       <ul className="sidebar__list">
-        {visibleData.map((item) => (
-          <li key={item.id} className="sidebar__item">
-            {item.title}
+        {visibleData?.map((item) => (
+          <li key={item?.id} className="sidebar__item">
+            {item?.title}
           </li>
         ))}
       </ul>
---
+++ src/types/url.types.ts
@@ -0,0 +1,3 @@
+export type StaticURL = string;
+export const JSONAPI: StaticURL =
+  "https://api.jsonbin.io/v3/b/63654b012b3499323bf58225";
